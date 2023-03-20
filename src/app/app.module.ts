20eb636f3404075c84911c44685c18689264cc95
igor2000xp <igorring@gmail.com>
2023-03-20 02:31:35 +0100

    init: add material.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaterialExampleModule } from '../material.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialExampleModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
